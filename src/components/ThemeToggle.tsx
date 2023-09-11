1a802118da4c3b29bc35c3e3ddbd661065e4e74b
Nabarun <nicknabarun@gmail.com>
2023-09-11 23:23:31 +0530

    Initial commit

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui";
+import { Icons } from "@/components";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/Dropdown";
+
+const ThemeToggle = () => {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant='ghost' size='sm' className='h-10'>
+          <Icons.Sun className='rotate-0 scale-100 transition-all hover:text-slate-900 dark:-rotate-90 dark:scale-0 dark:text-slate-400 dark:hover:text-slate-100' />
+          <Icons.Moon className='absolute rotate-90 scale-0 transition-all hover:text-slate-900 dark:rotate-0 dark:scale-100 dark:text-slate-400 dark:hover:text-slate-100' />
+          <span className='sr-only'>Toggle Theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent align='end' forceMount>
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          <Icons.Sun className='h-4 w-4 mr-2' />
+          <span>Light</span>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          <Icons.Moon className='h-4 w-4 mr-2' />
+          <span>Dark</span>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          <Icons.Laptop className='h-4 w-4 mr-2' />
+          <span>System</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ThemeToggle;
