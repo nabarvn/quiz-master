e7cdeeb25e8cea4a8f5d0ebe843ed41c2c1f8f38
Nabarun <nicknabarun@gmail.com>
2023-09-11 23:21:38 +0530

    Export `ThemeToggle`

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as SignInButton } from "./SignInButton";
 export { default as ProfileMenu } from "./ProfileMenu";
 export { default as UserAvatar } from "./UserAvatar";
 export { default as Icons } from "./Icons";
+export { default as ThemeToggle } from "./ThemeToggle";
