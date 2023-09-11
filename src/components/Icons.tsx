00c1007e5be1b740a034e1f050ec3034b4abfb94
Nabarun <nicknabarun@gmail.com>
2023-09-11 23:21:10 +0530

    Include `ThemeToggle` icons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,7 +1,10 @@
-import { User } from "lucide-react";
+import { User, Sun, Moon, Laptop } from "lucide-react";
 
 const Icons = {
   user: User,
+  Sun,
+  Moon,
+  Laptop,
 };
 
 export default Icons;
