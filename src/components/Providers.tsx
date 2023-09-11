18ac351ae0361fccbc59fdaf2209a7d7aa5d9c48
Nabarun <nicknabarun@gmail.com>
2023-09-11 23:23:14 +0530

    Implement `ThemeProvider` as a wrapper

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,9 +1,20 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
+import { ThemeProviderProps } from "next-themes/dist/types";
 
-const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+const Providers = ({ children, ...props }: ThemeProviderProps) => {
+  return (
+    <ThemeProvider
+      attribute='class'
+      defaultTheme='system'
+      enableSystem
+      {...props}
+    >
+      <SessionProvider>{children}</SessionProvider>
+    </ThemeProvider>
+  );
 };
 
 export default Providers;
