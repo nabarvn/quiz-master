960d8e912cb674ff57cac5d92f232dc55f27ee68
Nabarun <nicknabarun@gmail.com>
2023-09-12 22:39:46 +0530

    Add home page card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,47 @@
-import { Button } from "@/components/ui";
+import { Card } from "@/components/ui";
+import { SignInButton } from "@/components";
 
-export default function Home() {
+import {
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/Card";
+
+import Balancer from "react-wrap-balancer";
+import { getAuthSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await getAuthSession();
+
+  if (session) {
+    // user is logged in
+    return redirect("/dashboard");
+  }
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Button>Hello World</Button>
+    <main className='flex items-center justify-between p-24'>
+      <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+        <Card className='w-[300px] md:w-[400px]'>
+          <CardHeader>
+            <CardTitle className='md:text-xl'>
+              Welcome to Quiz Master! 🔥
+            </CardTitle>
+
+            <CardDescription>
+              <Balancer ratio={0.5}>
+                This platform offers an intuitive as well as user-friendly
+                interface, making quizzing fun and accessible for all.
+              </Balancer>
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <SignInButton text='Sign in with Google!' />
+          </CardContent>
+        </Card>
+      </div>
     </main>
   );
 }
