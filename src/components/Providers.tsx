1c45eff9a4cd35aa240624960a75299b52e3ffa9
Nabarun <nicknabarun@gmail.com>
2023-09-18 18:37:34 +0530

    Wrap children with `HydrationProvider`

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,17 +3,25 @@
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HydrationProvider } from "react-hydration-provider";
 
 const Providers = ({ children, ...props }: ThemeProviderProps) => {
+  const queryClient = new QueryClient();
+
   return (
-    <ThemeProvider
-      attribute='class'
-      defaultTheme='system'
-      enableSystem
-      {...props}
-    >
-      <SessionProvider>{children}</SessionProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute='class'
+        defaultTheme='system'
+        enableSystem
+        {...props}
+      >
+        <SessionProvider>
+          <HydrationProvider>{children}</HydrationProvider>
+        </SessionProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
