59705508f1a5f302e1a99217c3b8572dbef4b97f
Nabarun <nicknabarun@gmail.com>
2023-09-19 23:01:34 +0530

    Add `tailwind-scrollbar` plugin

--- src/lib/shadcn-preset.ts
+++ src/lib/shadcn-preset.ts
@@ -1,6 +1,7 @@
 import { Config } from "tailwindcss";
 import { shadcnPlugin } from "./shadcn-plugin";
 import animatePlugin from "tailwindcss-animate";
+import scrollbarPlugin from "tailwind-scrollbar";
 
 const shadcnPreset: Config = {
   darkMode: ["class"],
@@ -8,7 +9,7 @@ const shadcnPreset: Config = {
   // this array does not get merged with the default tailwind config file and hence it is safe to leave it empty
   // now technically 'shadcnPreset' is a valid config object (thanks to 'content' array) and therefore a valid preset
   content: [],
-  plugins: [animatePlugin, shadcnPlugin],
+  plugins: [animatePlugin, shadcnPlugin, scrollbarPlugin],
 };
 
 export { shadcnPreset };
