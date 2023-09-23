4fe555d16440fa7572c012357e669cf7ecd70108
Nabarun <nicknabarun@gmail.com>
2023-09-23 16:53:02 +0530

    Redesign the hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-import { Card } from "@/components/ui";
 import { SignInButton } from "@/components";
+import { Heading, Paragraph } from "@/components/ui";
 
-import {
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/Card";
-
-import Balancer from "react-wrap-balancer";
 import { getAuthSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import Balancer from "react-wrap-balancer";
 
 export default async function Home() {
   const session = await getAuthSession();
@@ -21,26 +14,24 @@ export default async function Home() {
   }
 
   return (
-    <main className='flex items-center justify-between p-24'>
-      <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-        <Card className='w-[300px] md:w-[400px]'>
-          <CardHeader>
-            <CardTitle className='md:text-xl'>
-              Welcome to Quiz Master! 🔥
-            </CardTitle>
+    <main className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+      <div className='flex flex-col gap-6 w-[300px] md:w-[500px] lg:w-[700px] xl:w-[900px] mt-10'>
+        <Heading
+          size='lg'
+          className='text-left text-slate-900 dark:text-slate-100'
+        >
+          <Balancer ratio={0.5}>
+            Unleash Your Inner Genius with Quiz Master.
+          </Balancer>
+        </Heading>
 
-            <CardDescription>
-              <Balancer ratio={0.5}>
-                This platform offers an intuitive as well as user-friendly
-                interface, making quizzing fun and accessible for all.
-              </Balancer>
-            </CardDescription>
-          </CardHeader>
+        <Paragraph className='text-left text-slate-900 dark:text-slate-100'>
+          <Balancer ratio={0.5}>
+            Test your knowledge and have fun with AI-powered quizzes.
+          </Balancer>
+        </Paragraph>
 
-          <CardContent>
-            <SignInButton text='Sign in with Google!' />
-          </CardContent>
-        </Card>
+        <SignInButton text='Get Started' />
       </div>
     </main>
   );
