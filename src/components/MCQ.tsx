e7dbe788026a0346def701fa3dcdfab9301db4d7
Nabarun <nicknabarun@gmail.com>
2023-09-25 16:02:14 +0530

    Handle `GPT` response error

--- src/components/MCQ.tsx
+++ src/components/MCQ.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import Link from "next/link";
-import axios from "axios";
 import { z } from "zod";
+import axios from "axios";
+import Link from "next/link";
 import { Game, Question } from "@prisma/client";
 import { differenceInSeconds } from "date-fns";
 import { cn, formatTimeDelta } from "@/lib/utils";
 
 import {
   Card,
+  CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
@@ -16,13 +17,13 @@ import {
 
 import { MCQCounter } from "@/components";
 import { useToast } from "@/hooks/use-toast";
-import { Button, buttonVariants } from "./ui/Button";
-import { BarChart, ChevronRight, Loader2, Timer } from "lucide-react";
+import { useMutation } from "@tanstack/react-query";
+import { useHydrated } from "react-hydration-provider";
 import { CheckAnswerValidator } from "@/lib/validators/answer";
 import { EndGameValidator } from "@/lib/validators/end-game";
-import { useMutation } from "@tanstack/react-query";
-import { useCallback, useEffect, useMemo, useState } from "react";
-import { useHydrated } from "react-hydration-provider";
+import { Button, buttonVariants } from "@/components/ui/Button";
+import { BarChart, ChevronRight, Loader2, Timer } from "lucide-react";
+import { Fragment, useCallback, useEffect, useMemo, useState } from "react";
 
 type MCQProps = {
   game: Game & {
@@ -181,81 +182,109 @@ const MCQ = ({ game }: MCQProps) => {
   return (
     hydrated && (
       <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-4xl w-[90vw] md:w-[80vw] mt-5 lg:mt-10 xl:mt-5'>
-        <div className='flex flex-row justify-between'>
-          <div className='flex flex-col'>
-            {/* topic */}
-            <p>
-              <span className='text-slate-400'>Topic</span> &nbsp;
-              <span className='text-white dark:text-slate-300 rounded-lg bg-slate-700 dark:bg-slate-950 px-2 py-1'>
-                {game.topic}
-              </span>
-            </p>
+        {currentQuestion && currentQuestion.options ? (
+          <Fragment>
+            <div className='flex flex-row justify-between'>
+              <div className='flex flex-col'>
+                {/* topic */}
+                <p>
+                  <span className='text-slate-400'>Topic</span> &nbsp;
+                  <span className='text-white dark:text-slate-300 rounded-lg bg-slate-700 dark:bg-slate-950 px-2 py-1'>
+                    {game.topic}
+                  </span>
+                </p>
 
-            <div className='flex self-start text-slate-400 mt-3'>
-              <Timer className='mr-2' />
-              {formatTimeDelta(differenceInSeconds(timeNow, game.timeStarted))}
-            </div>
-          </div>
+                <div className='flex self-start text-slate-400 mt-3'>
+                  <Timer className='mr-2' />
 
-          <MCQCounter
-            correctAnswers={stats.correctAnswers}
-            wrongAnswers={stats.wrongAnswers}
-          />
-        </div>
-
-        <Card className='w-full text-slate-700 dark:text-slate-300 mt-4'>
-          <CardHeader className='flex flex-row items-center'>
-            <CardTitle className='text-center divide-y divide-slate-900 dark:divide-slate-300 mr-5'>
-              <div>{questionIndex + 1}</div>
-
-              <div className='text-base'>{game.questions.length}</div>
-            </CardTitle>
-
-            <CardDescription className='flex-grow text-lg text-slate-700 dark:text-slate-300'>
-              {currentQuestion?.question}
-            </CardDescription>
-          </CardHeader>
-        </Card>
-
-        <div className='flex flex-col items-center justify-center w-full mt-4 mb-10'>
-          {options.map((option, index) => {
-            return (
-              <Button
-                key={index}
-                className='justify-start w-full border-slate-900 dark:border-slate-300 py-8 mb-4'
-                onClick={() => setSelectedChoice(index)}
-                variant={selectedChoice === index ? "default" : "outline"}
-              >
-                <div className='flex items-center justify-start'>
-                  <div
-                    className={
-                      selectedChoice === index
-                        ? "border border-slate-300 dark:border-slate-900 rounded-md w-10 p-2 px-3 mr-5"
-                        : "border border-slate-900 dark:border-slate-300 rounded-md w-10 p-2 px-3 mr-5"
-                    }
-                  >
-                    {index + 1}
-                  </div>
-
-                  <div className='text-start'>{option}</div>
+                  {formatTimeDelta(
+                    differenceInSeconds(timeNow, game.timeStarted)
+                  )}
                 </div>
-              </Button>
-            );
-          })}
+              </div>
 
-          <Button
-            size='default'
-            variant='default'
-            className='mt-2'
-            onClick={() => {
-              handleNext();
-            }}
-            disabled={isChecking || hasEnded}
-          >
-            {isChecking && <Loader2 className='w-4 h-4 mr-2 animate-spin' />}
-            Next <ChevronRight className='w-4 h-4 ml-2' />
-          </Button>
-        </div>
+              <MCQCounter
+                correctAnswers={stats.correctAnswers}
+                wrongAnswers={stats.wrongAnswers}
+              />
+            </div>
+
+            <Card className='w-full text-slate-700 dark:text-slate-300 mt-4'>
+              <CardHeader className='flex flex-row items-center'>
+                <CardTitle className='text-center divide-y divide-slate-900 dark:divide-slate-300 mr-5'>
+                  <div>{questionIndex + 1}</div>
+
+                  <div className='text-base'>{game.questions.length}</div>
+                </CardTitle>
+
+                <CardDescription className='flex-grow text-lg text-slate-700 dark:text-slate-300'>
+                  {currentQuestion?.question}
+                </CardDescription>
+              </CardHeader>
+            </Card>
+
+            <div className='flex flex-col items-center justify-center w-full mt-4 mb-10'>
+              {options.map((option, index) => {
+                return (
+                  <Button
+                    key={index}
+                    className='justify-start w-full border-slate-900 dark:border-slate-300 py-8 mb-4'
+                    onClick={() => setSelectedChoice(index)}
+                    variant={selectedChoice === index ? "default" : "outline"}
+                  >
+                    <div className='flex items-center justify-start'>
+                      <div
+                        className={
+                          selectedChoice === index
+                            ? "border border-slate-300 dark:border-slate-900 rounded-md w-10 p-2 px-3 mr-5"
+                            : "border border-slate-900 dark:border-slate-300 rounded-md w-10 p-2 px-3 mr-5"
+                        }
+                      >
+                        {index + 1}
+                      </div>
+
+                      <div className='text-start'>{option}</div>
+                    </div>
+                  </Button>
+                );
+              })}
+
+              <Button
+                size='default'
+                variant='default'
+                className='mt-2'
+                onClick={() => {
+                  handleNext();
+                }}
+                disabled={isChecking || hasEnded}
+              >
+                {isChecking && (
+                  <Loader2 className='w-4 h-4 mr-2 animate-spin' />
+                )}
+                Next <ChevronRight className='w-4 h-4 ml-2' />
+              </Button>
+            </div>
+          </Fragment>
+        ) : (
+          <Card className='mx-auto w-fit -mt-32'>
+            <CardHeader>
+              <CardTitle>GPT Response Error</CardTitle>
+
+              <CardDescription>
+                Please construct the quiz again.
+              </CardDescription>
+            </CardHeader>
+
+            <CardContent>
+              <Link
+                className={buttonVariants()}
+                href={"/quiz?topic=" + game.topic}
+              >
+                Retry
+              </Link>
+            </CardContent>
+          </Card>
+        )}
       </div>
     )
   );
