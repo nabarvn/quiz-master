4a297eb517d3c8c3f68d693475eb1859706c70ef
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-04-09 20:02:20 +0530

    Refactor history page layout and import `DashboardButton`

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,13 +1,9 @@
-import Link from "next/link";
+import { db } from "@/lib/db";
+import { Metadata } from "next";
+import { History } from "@/components";
 import { redirect } from "next/navigation";
 import { getAuthSession } from "@/lib/auth";
-import { History } from "@/components";
-
-import { db } from "@/lib/db";
-import { cn } from "@/lib/utils";
-import { Metadata } from "next";
-import { LucideLayoutDashboard } from "lucide-react";
-import { buttonVariants } from "@/components/ui/Button";
+import { DashboardButton } from "@/components/buttons";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/Card";
 
 export const metadata: Metadata = {
@@ -28,24 +24,17 @@ const HistoryPage = async () => {
   });
 
   return (
-    <main className='relative mx-auto min-h-screen max-w-7xl'>
-      <div className='absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-[340px] md:w-[390px] lg:w-[500px] -mt-14 lg:-mt-7 xl:-mt-14'>
-        <Card className='border-none shadow-none bg-slate-50 dark:bg-slate-900'>
-          <CardHeader className='px-0'>
-            <div className='flex items-center justify-between'>
-              <CardTitle className='text-2xl font-bold'>History</CardTitle>
-
-              <Link
-                href='/dashboard'
-                className={cn(buttonVariants({ size: "sm" }), "text-base")}
-              >
-                <LucideLayoutDashboard className='w-4 h-4 mr-2' />
-                Dashboard
-              </Link>
+    <main className="relative mx-auto min-h-screen max-w-7xl">
+      <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-[340px] md:w-[390px] lg:w-[500px] -mt-14 lg:-mt-7 xl:-mt-14">
+        <Card className="border-none shadow-none bg-slate-50 dark:bg-slate-900">
+          <CardHeader className="px-0">
+            <div className="flex items-center justify-between">
+              <CardTitle className="text-2xl font-bold">History</CardTitle>
+              <DashboardButton />
             </div>
           </CardHeader>
 
-          <CardContent className='relative overflow-y-auto border-2 rounded-sm h-[400px] bg-white dark:bg-slate-950 scrollbar-thin scrollbar-thumb-slate-300 dark:scrollbar-thumb-slate-500 scrollbar-thumb-rounded-sm'>
+          <CardContent className="relative overflow-y-auto border-2 rounded-sm h-[400px] bg-white dark:bg-slate-950 thin-scrollbar-thumb-gray thin-scrollbar-thumb-rounded thin-scrollbar-track-gray-lighter scrollbar-w-2 scrolling-touch pb-6 px-4 md:px-6">
             {gamesCount !== 0 ? (
               <History
                 getGames={async () => {
@@ -61,7 +50,7 @@ const HistoryPage = async () => {
                 }}
               />
             ) : (
-              <span className='absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 text-center'>
+              <span className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 text-center">
                 Nothing to display yet.
               </span>
             )}
