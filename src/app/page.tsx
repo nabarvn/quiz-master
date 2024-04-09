dc7e294c65fbf199a24aecc6fe2e900266e94408
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-04-09 20:05:36 +0530

    Refactor import paths for `SignInButton` component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { SignInButton } from "@/components";
+import { SignInButton } from "@/components/buttons";
 import { Heading, Paragraph } from "@/components/ui";
 
 import { getAuthSession } from "@/lib/auth";
@@ -15,35 +15,35 @@ export default async function Home() {
   }
 
   return (
-    <main className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-      <div className='flex flex-col items-center gap-6 w-[300px] md:w-[500px] lg:w-[700px] xl:w-[900px] mt-10'>
-        <div className='flex max-w-fit items-center justify-center rounded-full border border-slate-200 dark:border-slate-500 bg-white/75 dark:bg-slate-900 shadow-md backdrop-blur transition-all hover:border-slate-300 dark:hover:border-slate-700 px-5 py-1'>
+    <main className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="flex flex-col items-center gap-6 w-[300px] md:w-[500px] lg:w-[700px] xl:w-[900px] mt-10">
+        <div className="flex max-w-fit items-center justify-center rounded-full border border-slate-200 dark:border-slate-500 bg-white/75 dark:bg-slate-900 shadow-md backdrop-blur transition-all hover:border-slate-300 dark:hover:border-slate-700 px-5 py-1">
           <Link
-            target='_blank'
-            referrerPolicy='no-referrer'
-            href='https://github.com/nabarvn/quiz-master'
-            className='text-xs font-semibold text-slate-900 dark:text-slate-100'
+            target="_blank"
+            referrerPolicy="no-referrer"
+            href="https://github.com/nabarvn/quiz-master"
+            className="text-xs font-semibold text-slate-900 dark:text-slate-100"
           >
             Star on GitHub 🌟
           </Link>
         </div>
 
         <Heading
-          size='lg'
-          className='text-center text-slate-900 dark:text-slate-100'
+          size="lg"
+          className="text-center text-slate-900 dark:text-slate-100"
         >
           <Balancer ratio={0.5}>
             Unleash Your Inner Genius with Quiz Master.
           </Balancer>
         </Heading>
 
-        <Paragraph className='text-center text-slate-900 dark:text-slate-100'>
+        <Paragraph className="text-center text-slate-900 dark:text-slate-100">
           <Balancer ratio={0.5}>
             Test your knowledge and have fun with AI-powered quizzes.
           </Balancer>
         </Paragraph>
 
-        <SignInButton text='Get Started' />
+        <SignInButton text="Get Started" />
       </div>
     </main>
   );
