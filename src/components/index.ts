b7d4b85c6d547c62477c59fd87a20db521bb11e3
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-04-09 20:20:34 +0530

    Remove `SignInButton` export from components index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { default as Navbar } from "./Navbar";
 export { default as Providers } from "./Providers";
-export { default as SignInButton } from "./SignInButton";
 export { default as ProfileMenu } from "./ProfileMenu";
 export { default as UserAvatar } from "./UserAvatar";
 export { default as Icons } from "./Icons";
