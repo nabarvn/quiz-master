24785346692b1683375493791d68fdf4ba142e31
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-06-03 16:50:31 +0530

    Refactor HotTopics component to utilize implicit return for improved readability and simplicity

--- src/components/dashboard/HotTopics.tsx
+++ src/components/dashboard/HotTopics.tsx
@@ -8,22 +8,18 @@ import {
   CardTitle,
 } from "@/components/ui/Card";
 
-const HotTopics = () => {
-  return (
-    <Card className='col-span-4 h-[315px] md:h-[630px] lg:h-[500px] xl:h-[583px]'>
-      <CardHeader>
-        <CardTitle className='text-xl font-bold'>Hot Topics</CardTitle>
+const HotTopics = () => (
+  <Card className="col-span-4 h-[315px] md:h-[630px] lg:h-[500px] xl:h-[583px]">
+    <CardHeader>
+      <CardTitle className="text-xl font-bold">Hot Topics</CardTitle>
 
-        <CardDescription>
-          Initiate a quiz by clicking on a topic.
-        </CardDescription>
-      </CardHeader>
+      <CardDescription>Initiate a quiz by clicking on a topic.</CardDescription>
+    </CardHeader>
 
-      <CardContent>
-        <WordCloud />
-      </CardContent>
-    </Card>
-  );
-};
+    <CardContent>
+      <WordCloud />
+    </CardContent>
+  </Card>
+);
 
 export default HotTopics;
