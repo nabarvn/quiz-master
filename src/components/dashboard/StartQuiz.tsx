d966c34d3ed04723e164d4c81d6789adcfe96d2d
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-06-03 16:50:17 +0530

    Refactor StartQuiz component to update quiz navigation handling

--- src/components/dashboard/StartQuiz.tsx
+++ src/components/dashboard/StartQuiz.tsx
@@ -1,33 +1,28 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { BrainCircuit } from "lucide-react";
 import { Card } from "@/components/ui";
 import Balancer from "react-wrap-balancer";
+import { BrainCircuit } from "lucide-react";
 import { CardContent, CardHeader, CardTitle } from "@/components/ui/Card";
 
-const StartQuiz = () => {
-  const router = useRouter();
+const StartQuiz = () => (
+  <Card
+    className="hover:cursor-pointer hover:opacity-75"
+    onClick={() => (window.location.href = "/quiz")}
+  >
+    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+      <CardTitle className="text-xl font-bold">Start Quiz!</CardTitle>
+      <BrainCircuit size={21} strokeWidth={2.5} />
+    </CardHeader>
 
-  return (
-    <Card
-      className='hover:cursor-pointer hover:opacity-75'
-      onClick={() => router.push("/quiz")}
-    >
-      <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-        <CardTitle className='text-xl font-bold'>Start Quiz!</CardTitle>
-        <BrainCircuit size={21} strokeWidth={2.5} />
-      </CardHeader>
-
-      <CardContent>
-        <p className='text-sm text-muted-foreground'>
-          <Balancer ratio={0.5}>
-            Challenge yourself to a quiz with a topic of your choice.
-          </Balancer>
-        </p>
-      </CardContent>
-    </Card>
-  );
-};
+    <CardContent>
+      <p className="text-sm text-muted-foreground">
+        <Balancer ratio={0.5}>
+          Challenge yourself to a quiz with a topic of your choice.
+        </Balancer>
+      </p>
+    </CardContent>
+  </Card>
+);
 
 export default StartQuiz;
