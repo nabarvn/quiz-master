01e0306c4bfb5b135686f54dab22ed31d418a700
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-06-03 16:51:57 +0530

    Change the navigation method in AttemptHistory component to enhance user experience

--- src/components/dashboard/AttemptHistory.tsx
+++ src/components/dashboard/AttemptHistory.tsx
@@ -1,30 +1,25 @@
 "use client";
 
 import { Card } from "@/components/ui";
-import { CardContent, CardHeader, CardTitle } from "@/components/ui/Card";
 import { History } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { CardContent, CardHeader, CardTitle } from "@/components/ui/Card";
 
-const AttemptHistory = () => {
-  const router = useRouter();
+const AttemptHistory = () => (
+  <Card
+    className="hover:cursor-pointer hover:opacity-75"
+    onClick={() => (window.location.href = "/history")}
+  >
+    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+      <CardTitle className="text-xl font-bold">History</CardTitle>
+      <History size={21} strokeWidth={2.5} />
+    </CardHeader>
 
-  return (
-    <Card
-      className='hover:cursor-pointer hover:opacity-75'
-      onClick={() => router.push("/history")}
-    >
-      <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-        <CardTitle className='text-xl font-bold'>History</CardTitle>
-        <History size={21} strokeWidth={2.5} />
-      </CardHeader>
-
-      <CardContent>
-        <p className='text-sm text-muted-foreground'>
-          View all previous quiz attempts.
-        </p>
-      </CardContent>
-    </Card>
-  );
-};
+    <CardContent>
+      <p className="text-sm text-muted-foreground">
+        View all previous quiz attempts.
+      </p>
+    </CardContent>
+  </Card>
+);
 
 export default AttemptHistory;
