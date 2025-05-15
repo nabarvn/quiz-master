ea09e50389b79fe83b6e630c1a92532978431bdd
Nabarun <64539836+nabarvn@users.noreply.github.com>
2025-05-15 18:36:55 -0400

    Add function to generate default metadata for SEO and social sharing

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
-import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -24,4 +25,56 @@ export function formatTimeDelta(seconds: number) {
   }
 
   return parts.join(" ");
+}
+
+export function constructMetadata({
+  title = "Quiz Master - The Smart Way to Learn",
+  description = "Quiz Master offers a fun and engaging way to learn and challenge yourself. Test your knowledge on any subject with AI-generated quizzes.",
+  image = "/thumbnail.png",
+  icons = [
+    {
+      rel: "icon",
+      url: "/favicon.ico", // for standard browsers
+    },
+    {
+      rel: "apple-touch-icon",
+      url: "/icon.png", // for Apple devices
+    },
+  ],
+  noIndex = false, // allow search engine bots to crawl and index the website
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: Metadata["icons"];
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@nabarvn",
+    },
+    icons,
+    metadataBase: new URL("https://quiz.nabarun.app"),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
 }
